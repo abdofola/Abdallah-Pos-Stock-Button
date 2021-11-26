77c0d1496a79a0a4476ad81b928e5bae1b9932f7
abdofola <abdofola67@gmail.com>
2021-11-27 01:30:31 +0200

    finally discover that the product id is located in the dataset obj

--- static/src/js/pos_custom.js
+++ static/src/js/pos_custom.js
@@ -1,33 +1,24 @@
 odoo.define("pos_custom.custom", function (require) {
   "use strict";
   const PosComponent = require("point_of_sale.PosComponent");
-  //   const ProductScreen = require("point_of_sale.ProductScreen");
+  //   const ProductScreen = require("point_of_sale.ProductScreen"); no need for this component in this case, cuz we're not gonna add it to the keyboard(product screen).
   const Registries = require("point_of_sale.Registries");
 
   class PosInventoryBtn extends PosComponent {
-    async onClick() {
-      const { selected_orderline } = this.env.pos.get_order();
+    async onClick(evt) {
+      const { productId } = evt.target.parentElement.parentElement.dataset;
+      const domain = [["id", "=", parseInt(productId)]];
+      const [product] = await this.rpc({
+        model: "product.product",
+        method: "search_read",
+        args: [domain, ["name", "qty_available"]],
+      });
+      const quantity =
+        product.qty_available <= 0 ? "None left !" : product.qty_available;
 
-      if (selected_orderline) {
-        const { id } = selected_orderline.product;
-        const domain = [["id", "=", id]];
-        const [result] = await this.rpc({
-          model: "product.product",
-          method: "search_read",
-          args: [domain, ["name", "qty_available"]],
-        });
-        const quantity =
-          result.qty_available <= 0 ? "None left !" : result.qty_available;
-
-        return this.showPopup("ConfirmPopup", {
-          title: `Available ${result.name} in the stock`,
-          body: quantity,
-        });
-      }
-
-      //   when there's no orderline selected; return ErrorPopup.
-      return this.showPopup("ErrorPopup", {
-        title: "Please select orderline first !",
+      return this.showPopup("ConfirmPopup", {
+        title: `Available ${product.name} in the stock`,
+        body: quantity,
       });
     }
   }
