81cf85b8adebc5401325cccc0f7fdc106a36ac52
abdofola <abdofola67@gmail.com>
2021-11-27 03:02:22 +0200

    remove unnecessary comments

--- static/src/js/pos_custom.js
+++ static/src/js/pos_custom.js
@@ -1,7 +1,6 @@
 odoo.define("pos_custom.custom", function (require) {
   "use strict";
   const PosComponent = require("point_of_sale.PosComponent");
-  //   const ProductScreen = require("point_of_sale.ProductScreen"); no need for this component in this case, cuz we're not gonna add it to the keyboard(product screen).
   const Registries = require("point_of_sale.Registries");
 
   class PosInventoryBtn extends PosComponent {
