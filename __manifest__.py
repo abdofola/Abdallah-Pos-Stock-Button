b837d033dcd36465707a47f1d6bd70157d7f5fc1
abdofola <abdofola67@gmail.com>
2021-11-27 02:33:55 +0200

    updating manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -2,9 +2,10 @@
 {
     'name': "POS Custom",
     'summary': "Point of sale custom module",
-    'description': """Long description""",
+    'description': """This module adds custom button to the point of sale
+        UI to display the available quantity of a product in the stock.""",
     'author': "Abdallah Alkaser",
-    'website': "http://www.example.com",
+    'website': "http://abdallah-alkaser.netlify.app",
     'category': 'Point of Sale',
     'version': '14.0.1',
     'depends': ['point_of_sale'],
